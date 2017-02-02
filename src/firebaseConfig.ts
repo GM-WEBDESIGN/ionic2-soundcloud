d2f396624b23b08d401f7965e8ebceea36107091
Germain <gmichaud.pro@gmail.com>
2017-02-02 17:23:44 +0100

    Update firebaseConfig.ts

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyC99UqVytxSQacyYluhawUu54gBZzZCNxU",
-    authDomain: "musicapp-a95ea.firebaseapp.com",
-    databaseURL: "https://musicapp-a95ea.firebaseio.com",
-    storageBucket: "musicapp-a95ea.appspot.com",
-    messagingSenderId: "328098877568"
+    apiKey: "<API_KEY>",
+    authDomain: "<AUTH_DOMAIN>",
+    databaseURL: "<DATABASE_URL>",
+    storageBucket: "<STORAGE_BUCKET>",
+    messagingSenderId: "<MESSAGING_SENDER_ID>"
 }
